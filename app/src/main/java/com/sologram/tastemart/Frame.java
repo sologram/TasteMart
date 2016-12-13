549b1a6b95a47d1f504129293fb8d49d2b754123
hans <sologram@gmail.com>
2016-12-13 14:59:08 +0800

    Initial commit

---
+++ app/src/main/java/com/sologram/tastemart/Frame.java
@@ -0,0 +1,13 @@
+package com.sologram.tastemart;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Frame extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.frame);
+	}
+}
